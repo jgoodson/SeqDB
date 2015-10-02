49c0da48ea31c2a06c2374f0359caa9a3b6d6d6a
Jonathan Goodson <jgoodson@umd.edu>
2015-10-02 14:11:51 -0400

    Added setup.py for version 0.1

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SeqDB',
+      version='0.1',
+      description='UniprotKB local "mirror" tools',
+      author='Jonathan Goodson',
+      author_email='jgoodson@umd.edu',
+      package_dir={'SeqDB': ''},
+      packages=['SeqDB'],
+      )
+
