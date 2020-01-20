e5fa3fa23b8fb3d5f27eb9babad3a4d3158dcb11
Jonathan <jgoodson@umd.edu>
2020-01-20 13:14:16 -0500

    Slight efficiency boost

--- UniprotDB/MongoDB.py
+++ UniprotDB/MongoDB.py
@@ -174,7 +174,7 @@ class MongoDatabase(object):
         with tqdm(total=total, smoothing=0.1, disable=(not loud)) as pbar:
             for record in raw_protein_records:
                 if len(tasks)>n:
-                    done, pending = await asyncio.wait(tasks)
+                    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
                     for d in done:
                         tasks.remove(d)
                     if len(pending)>n:
